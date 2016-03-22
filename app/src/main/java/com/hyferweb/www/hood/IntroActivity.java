27f477f4a6f0480d8845262b7a0afe236e4016f1
Mohit Kishore <gkmohit@gmail.com>
2016-03-22 18:15:39 -0400

    added intro activity

--- app/src/main/java/com/hyferweb/www/hood/IntroActivity.java
+++ app/src/main/java/com/hyferweb/www/hood/IntroActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class IntoActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_into);
+        setContentView(R.layout.activity_intro);
     }
 
     @Override
